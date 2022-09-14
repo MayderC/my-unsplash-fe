ac6247aad34817a05ebb95c5e4f3d00150e2d584
Mayder C <maydercortes1@gmail.com>
2022-09-14 10:32:12 -0600

    hotfix, use base_url

--- src/services/images.services.ts
+++ src/services/images.services.ts
@@ -2,13 +2,13 @@ import type { ImageRequest, ImageResponse } from "@/interfaces/images"
 const BASE_URL = import.meta.env.BASE_URL
 
 export const getImages = async () : Promise<ImageResponse[]> => {
-  const res = await fetch('http://localhost:3000/api/image')
+  const res = await fetch(BASE_URL)
   return await res.json()
 }
 
 export const saveImage = async (image: ImageRequest): Promise<ImageResponse> => {
 
-  const res = await  fetch('http://localhost:3000/api/image', {
+  const res = await  fetch(BASE_URL, {
     method: 'POST',
     headers: {
       'content-type' : 'application/json',
@@ -21,7 +21,7 @@ export const saveImage = async (image: ImageRequest): Promise<ImageResponse> => 
 
 export const deleteImage = async (id: string) : Promise<void> => {
 
-   await  fetch('http://localhost:3000/api/image/' + id, {
+   await  fetch(BASE_URL + id, {
     method: 'DELETE',
     headers: {
       'content-type' : 'application/json',
